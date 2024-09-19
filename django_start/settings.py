6274911b517297bb17b496f2c358e8ed41cde838
Rishabh Suny <rishabhsunyriz@gmail.com>
2024-09-19 10:50:13 +0000

    enable serving of image files

--- django_start/settings.py
+++ django_start/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'hello_world',
     'about',
     'Perfumes',
@@ -95,7 +96,7 @@ WSGI_APPLICATION = 'django_start.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': dj_database_url.parse("DATABASE_URL")}
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
