8c6cd3aad5b7dfac848ad64a078b4ab1f941dfd1
Rishabh Suny <rishabhsunyriz@gmail.com>
2024-09-23 11:47:26 +0000

    add blog_detail slug

--- Perfumes/admin.py
+++ Perfumes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Perfume, Review
+
+@admin.register(Perfume)
+
+
 
 # Register your models here.
+admin.site.register
--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ComparisonList.as_view(), name='home'),
+    path('<slug:slug>', views.blog_detail.html, name="blog_detai")
 ]
