b1007ccd4162424848f36e2512adf45a6ee7ca71
Rishabh Suny <rishabhsunyriz@gmail.com>
2024-09-23 09:05:00 +0000

    set up views

--- Perfumes/views.py
+++ Perfumes/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.views.generic import TemplateView
+from django.views import generic
+from .forms import ReviewForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+frrom django.db.models import Avg
 # Create your views here.
+
+class PerfumesList(generic.ListView)
+    queryset = Perfume.objects.all()
+    template_name = "index.html"
