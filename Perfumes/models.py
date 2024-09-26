c66a53415306ffc00c52f3d5313e394e3a33e15c
Rishabh Suny <rishabhsunyriz@gmail.com>
2024-09-26 10:26:28 +0000

    html removed and adding elements to style.css

--- Perfumes/models.py
+++ Perfumes/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 from cloudinary.models import CloudinaryField
-
-STATUS = ()
+from django.db.models.fields import DecimalField, FloatField, IntegerField
+STATUS = ((0 , "Draft") , (1 ,"Published"))
 # Create your models here.
 class Perfume(models.Model):
         featured_image = CloudinaryField('image', default='placeholder')
+        
 
 class Review(models.Model):
         featured_image = CloudinaryField('image', default='placeholder')
@@ -14,3 +15,36 @@ class Review(models.Model):
 
 class Meta:
         ordering = ["-created_on"]
+
+# related_query-__name__
+# related _name 
+# limit_choices_to
+# choice_set 
+# models.py 
+# from django.db import models
+# from django.utils import timezone
+# from autoslug import AutoSlugField
+# forms.py
+# from django import forms
+# from django.forms import ModelForm
+# from app.models import UserProfile
+# all 
+# from django.contrib.auth.models import User
+
+
+
+# Class UserProfile(models.Model)
+#     user = models.OneToOneField(User)
+#     website = models.URLField(blank=True)
+#     picture = models.ImageField(uploade_to='media/' , blank=True)
+
+#     def __unicode_(self):
+#         return self.user.username
+
+
+#     model = User
+#     fields = ('username ' , 'email' , 'password')
+# class UserProfileForm(forms)
+#     class Meta:
+#     model = UserProfile
+#     fields = ('website' , 'picture')
--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,15 @@ from django.db import models
 # Create your models here.
 class Poster(models.Model):
         title = models.CharField(max_length=50)
+        name = models.CharField(max_length=100)
+        tagline = models.TextField()
         # featured_image = CloudinaryField('image', default='placeholder')
+ #
+        #     class Target(models.Model):
+        #         model = models.IntegerField()
+        #         model_set = models.IntegerField()
+        #
+        #     class Model(models.Model):
+        #         foreign = models.ForeignKey(Target)
+        #         m2m = models.ManyToManyField(Target)
+        
