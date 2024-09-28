ee8e4a9100ee818bf2d38fec80485639c00268e1
Rishabh Suny <rishabhsunyriz@gmail.com>
2024-09-28 08:00:25 +0000

    previous changes

--- Perfumes/urls.py
+++ Perfumes/urls.py
@@ -4,18 +4,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 urlpatterns = [
     
-   path('', views.PerfumesList.as_view(), name='Perfumes'),
-   path('', views.DuplicateList.as_view(), name='Duplicates'),
+   path('perfumes', views.PerfumesList.as_view(), name='Perfumes'),
+   path('duplicates', views.DuplicateList.as_view(), name='Duplicates'),
      path(
         '<slug:slug>/edit_review/<int:review_id>',
         views.review_edit,
         name='review_edit'
     ),
+   #  edit reviews
     path(
         '<slug:slug>/delete_review/<int:review_id>',
         views.review_delete,
         name='review_delete'
     ),
+    # delete review
 ]
 
 # added urls to view home review_edit in views and review_delete
