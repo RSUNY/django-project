e9c4bcbfe467730fa0d5da9be2c356f96309dad4
Rishabh Suny <rishabhsunyriz@gmail.com>
2024-09-28 10:44:19 +0000

    saved files html

--- Perfumes/forms.py
+++ Perfumes/forms.py
@@ -1,1 +1,17 @@
 from django import forms
+from .models import Review
+
+class ReviewForm(forms.ModelForm):
+    RATING_CHOICES = [(i, str(i)) for i in range(1, 6)]
+
+class Meta:
+    model = Review
+    fields = ('review_title', 'review_content', 'rating',)
+    widgets = {
+        'review_title': forms.TextInput(
+             attrs={'class': 'form-control',
+                'placeholder': 'Enter your review title'
+                                'Enter you review context'
+                }
+            ),
+        }
--- Perfumes/views.py
+++ Perfumes/views.py
@@ -4,6 +4,7 @@ from django.views import generic
 from .models import Perfume, Duplicate, Review 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .forms import ReviewForm
 from django.http import HttpResponseRedirect
 # Create your views here.
 
@@ -32,7 +33,22 @@ class DuplicateDetail(generic.DetailView):
     context_object_name = 'duplicate'
     paginate_by = 8
 
-def PerfumeDetail(request, slug):
+
+@login_required
+def review_edit(request, slug, review_id):
+    review = get_object_or_404(Review, id=review_id)
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+    if form.is_valid():
+        form.save()
+    return redirect('perfume_detail', slug=slug)
+    else:
+        form = ReviewForm(instance=review)
+    return redirect('perfume_detail',
+slug=slug)
+    else:
+        form = ReviewForm(instance=review)
+    return render(request, 'review_edit.html', {'form': form})
     """
     Display an individual :model:`Perfumes.Review`.
 
